8469804f9bec36bcfcf3583b067b7ba4242f458d
holgadoM <holgado.m@gmail.com>
2022-09-14 10:57:47 -0300

    testing features and API format response

--- src/test/current.test.js
+++ src/test/current.test.js
@@ -1,50 +1,55 @@
 const supertest = require('supertest')
 const build = require('../app')
-const objCity = require('./mock/obj_city')
+const { current_get_information } = require('../services/current.service')
+
+const my_ip = '181.46.66.9'
+
+const app = build()
+
+beforeAll( async ()=>{
+    await app.ready()
+})
 
 describe('Test /current[/city]',()=>{
 
-    test('Response with city_information', async ()=>{
-        const app = build()
-        await app.ready()
-
-        const res = await supertest(app.server)
-        .get(`/v1/current/${objCity['name']}`)
-        .expect(200)
-
-        expect(res.body).toHaveProperty('city_information')
-    })
-
-    test('Response with city_information and information inside', async ()=>{
-        const app = build()
-        await app.ready()
-
-        const res = await supertest(app.server)
-        .get(`/v1/current/${objCity['name']}`)
-        .expect(200)
-
-        expect(res.body).toHaveProperty('city_information')
-        expect(res.body['city_information'].length).toBeGreaterThanOrEqual(1)
-    })
-
-    test('Response with ip_information',async () =>{
-        const app = build()
-        await app.ready()
-        const res = await supertest(app.server)
-            .get('/v1/current/')
+    test('API response Json format', async ()=>{
+        const city = 'Buenos aires'
+        await supertest(app.server)
+            .get(`/v1/current/${city}`)
             .expect(200)
-
-        expect(res.body).toHaveProperty('ip_information')
+            .expect('Content-type',/application\/json/)
     })
 
-    test('Response with weather_information',async () =>{
-        const app = build()
-        await app.ready()
-        const res = await supertest(app.server)
-            .get('/v1/current/')
-            .expect(200)
-        expect(res.body).toHaveProperty('weather_information')
-        expect(res.body['weather_information']).toHaveProperty('current')
+    test('send city param and response with two objects => city_information and weather_information  ', async ()=>{
+        const city = 'Buenos aires'
+        const res = await current_get_information(city, my_ip)
+
+        expect(res).toHaveProperty('city_information')
+        expect(res).toHaveProperty('weather_information')
+    })
+
+    test('send null city param and response with two objects => ip_information and weather_information  ', async ()=>{
+        const city = null
+        const res = await current_get_information(city, my_ip)
+
+        expect(res).toHaveProperty('ip_information')
+        expect(res).toHaveProperty('weather_information')
+    })
+
+    test('send undefined city param and response with two objects => ip_information and weather_information  ', async ()=>{
+        const city = undefined
+        const res = await current_get_information(city, my_ip)
+
+        expect(res).toHaveProperty('ip_information')
+        expect(res).toHaveProperty('weather_information')
+    })
+
+    test('send empty string into city param and response with two objects => ip_information and weather_information  ', async ()=>{
+        const city = ''
+        const res = await current_get_information(city, my_ip)
+
+        expect(res).toHaveProperty('ip_information')
+        expect(res).toHaveProperty('weather_information')
     })
 
     
