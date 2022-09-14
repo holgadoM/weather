b8baa8db3c2447b1297a00ce6f914037494d5f82
holgadoM <holgado.m@gmail.com>
2022-09-14 10:08:20 -0300

    refactor /forecast[/:city]
    extracted controller logic and moved to a service

--- src/controllers/v1/forecast.js
+++ src/controllers/v1/forecast.js
@@ -1,32 +1,14 @@
-const { getIpInformation } = require("../../utils/ip_information")
-const { getCityInformation, getExtendedForecast } = require("../../utils/weather_api")
+const { forecast_get_information } = require("../../services/v1/forecast.service")
 
 const forecast = async (req, reply)=>{
     const {city} = req.params
+    const ip = req.socket.remoteAddress
 
     try {
 
-        var objResponse = {};
+        const response = await forecast_get_information(city,ip)
 
-        if(!city || city === 'null' || city === 'undefined' ){
-            const ip = req.socket.remoteAddress
-            
-            const ip_information = await getIpInformation(ip)
-            objResponse['ip_information'] = ip_information
-            if( ip_information['status'] === 'success' ){
-                const forecast_information = await getExtendedForecast(ip_information['lat'], ip_information['lon'])
-                objResponse['forecast_information'] = forecast_information
-            }
-        }else{
-            const city_information = await getCityInformation(city)
-            objResponse['city_information'] = city_information
-            if( city_information.length > 0 ){
-                const forecast_information = await getExtendedForecast(city_information[0]['lat'], city_information[0]['lon'])
-                objResponse['forecast_information'] = forecast_information
-            }
-        }
-
-        reply.send({...objResponse})
+        reply.send(response)
         
     } catch (error) {
         console.log(error)
